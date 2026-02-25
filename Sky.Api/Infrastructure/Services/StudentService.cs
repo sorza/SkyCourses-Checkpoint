73a460bde03058c609fdad2ac1d54272647ca295
sorza <alexandre.sorza@outlook.com>
2026-02-24 21:22:33 -0300

    feat: implementando studentservice

---
+++ Sky.Api/Infrastructure/Services/StudentService.cs
@@ -0,0 +1,35 @@
+﻿using Sky.Api.Application.Interfaces;
+using Sky.Api.Application.Requests.Students;
+using Sky.Api.Application.Responses;
+using Sky.Api.Application.Responses.Students;
+
+namespace Sky.Api.Infrastructure.Services
+{
+    public class StudentService : IStudentService
+    {
+        public async Task<Response<StudentResponse>?> CreateStudentAsync(CreateStudentRequest request, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Response<StudentResponse>?> DeleteStudentAsync(int id, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<PagedResponse<IEnumerable<StudentResponse>>?> GetAllStudentsAsync(GetAllStudentsRequest request, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Response<StudentResponse>?> GetStudentByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Response<StudentResponse>?> UpdateStudentAsync(int id, CreateStudentRequest request, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
